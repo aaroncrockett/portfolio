06e07f17bb4acb43f61a7efb270565b442091f52
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-08-21 19:13:47 -0700

    logo component

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,5 +1,13 @@
 <script>
 	import logo from '$lib/images/logo-me.svg';
+	let sizeProp = 'lg';
+
+	const lgClass = 'w-20';
+	const smClass = 'w-4';
+
+	$: chosenSizeClass = sizeProp == 'lg' ? lgClass : smClass;
+
+	$: totalElClass = `${chosenSizeClass} h-auto`;
 </script>
 
-<img src={logo} alt="Aaron Crockett" />
+<img src={logo} alt="Aaron Crockett" class={totalElClass} />
