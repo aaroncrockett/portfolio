fcb27949cc3224284588900e4ca9c394cef2b8f4
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-08-21 18:07:40 -0700

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,41 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { LightboxGallery, GalleryThumbnail, GalleryImage } from 'svelte-lightbox';
+
+	const images = [
+		{ src: 'https://picsum.photos/200/300', width: 200, height: 300 },
+		{ src: 'https://picsum.photos/200/300', width: 200, height: 300 },
+		{ src: 'https://picsum.photos/200/300', width: 200, height: 300 },
+		{ src: 'https://picsum.photos/200/300', width: 200, height: 300 }
+	];
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Aaron Crockett: Artist, Designer, UI Engineer.</title>
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<h1>This website is being redesigned and developed.</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<p>images:</p>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+	<LightboxGallery>
+		<!-- Layout-->
+		<svelte:fragment slot="thumbnail">
+			<div class="sample-class-1">
+				<GalleryThumbnail>
+					<img src="https://picsum.photos/100/100" alt="Simple lightbox" />
+				</GalleryThumbnail>
+			</div>
+		</svelte:fragment>
 
-	<Counter />
+		<GalleryImage>
+			<img src="https://picsum.photos/200/300" alt="Simple lightbox" />
+		</GalleryImage>
+		<!-- <GalleryImage>
+			<img src="./image-1.jpg" alt="Simple lightbox" />
+		</GalleryImage>
+		<GalleryImage>
+			<img src="./image-2.jpg" alt="Simple lightbox" />
+		</GalleryImage> -->
+	</LightboxGallery>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
